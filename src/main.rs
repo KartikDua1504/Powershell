be42a1151dd11254fcc58f796e9e30b3544cb263
KartikDua1504 <duakartik40@gmail.com>
2025-03-30 04:34:42 +0530

    Second Task

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::io::{self, Write};
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+    let stdin = io::stdin();
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
+    println!("{}: command not found",input.trim());
 }
