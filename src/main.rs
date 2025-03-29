4be6cbf0fd0c9c37474e0da725d419bc86a03992
KartikDua1504 <duakartik40@gmail.com>
2025-03-30 04:27:23 +0530

    First

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-
 fn main() {
-    // Uncomment this block to pass the first stage
-    // print!("$ ");
-    // io::stdout().flush().unwrap();
-
-    // Wait for user input
+    print("$ ");
+    io::stdout().flush().unwrap();
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
 }
