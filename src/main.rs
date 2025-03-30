0e7abbb3b825861d1d8fd749b0cba89b667803c8
KartikDua1504 <duakartik40@gmail.com>
2025-03-30 18:04:10 +0530

    seventh

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,48 @@
-#[allow(unused_imports)]
+mod builtins;
+use crate::builtins::COMMANDS;
+use pathsearch::find_executable_in_path;
 use std::io::{self, Write};
+
 fn main() {
     let stdin = io::stdin();
     let mut stdout = io::stdout();
     let mut input = String::new();
+
     loop {
         print!("$ ");
         stdout.flush().unwrap();
+
         if stdin.read_line(&mut input).is_err() {
             break;
         }
-        let trimmed = input.trim().to_owned();
-        input.clear(); 
+
+        let trimmed = input.trim();
+        input.clear();
+
         if trimmed.is_empty() {
             continue;
         }
-        match trimmed.split_once(' ') {
-            Some((command, args)) => match command {
-                "exit" if args == "0" => break,
-                "echo" => println!("{}", args),
-                "type" => match args {
-                    "echo" | "exit" | "type" => println!("{} is a shell builtin", args),
-                    _ => println!("{}: not found", args),
-                },
-                _ => println!("{}: command not found", trimmed),
-            },
-            None => println!("{}: command not found", trimmed),
+
+        let tokens: Vec<&str> = trimmed.split_whitespace().collect();
+
+        match tokens.as_slice() {
+            ["exit"] => std::process::exit(0),
+            ["exit", code] => {
+                if let Ok(num) = code.parse::<i32>() {
+                    std::process::exit(num);
+                }
+            }
+            ["echo", ..] => println!("{}", tokens[1..].join(" ")),
+            ["type", cmd] => {
+                if COMMANDS.contains(cmd) {
+                    println!("{} is a shell builtin", cmd);
+                } else if let Some(path) = find_executable_in_path(cmd) {
+                    println!("{} is {}", cmd, path.to_str().unwrap());
+                } else {
+                    println!("{}: not found", cmd);
+                }
+            }
+            _ => println!("{}: command not found", trimmed),
         }
     }
 }
